b31e5c00eea31c3c04f51da93ae949a8256ff410
Rupesh <dasrupesh2124@gmail.com>
2025-07-09 17:57:23 +0530

    Isolated logic of getting and showing

--- app.py
+++ app.py
@@ -9,21 +9,28 @@ Your selection: """
 
 WELCOME = "Welcome to the programming diary!"
 
+
+def prompt_new_entry():
+    entry_content = input("What have you learned today? ")
+    entry_date = input("Enter the date: ")
+
+    add_entry(entry_content, entry_date)
+
+
+def view_entries(entries):
+    for entry in entries:
+        print(f"{entry['date']} \n {entry['content']} \n\n")
+
+
 print(WELCOME)
 
 while (user_input := input(menu)) != "3":
     if user_input == "1":
         print("Adding...")
-
-        entry_content = input("What have you learned today? ")
-        entry_date = input("Enter the date: ")
-
-        add_entry(entry_content, entry_date)
+        prompt_new_entry()
     elif user_input == "2":
         print("Viewing...")
-
         entries = get_entries()
-        for entry in entries:
-            print(f"{entry['date']} \n {entry['content']} \n\n")
+        view_entries(entries)
     else:
         print("Invalid option, please try again!")
