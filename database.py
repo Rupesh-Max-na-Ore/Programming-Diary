dabe3398c1b70651cf5b98d204d6d043bbaffa2d
Rupesh <dasrupesh2124@gmail.com>
2025-07-10 00:08:22 +0530

    Retrieving entries list from cursor completed, app CLI functionality completed

--- database.py
+++ database.py
@@ -10,20 +10,14 @@ def create_table():
         )
 
 
-# entries = []
-
-
 def add_entry(entry_content, entry_date):
-    # entries.append({"content": entry_content, "date": entry_date})
     with connection:
         connection.execute(
             "INSERT INTO entries VALUES (?,?);", (entry_content, entry_date)
         )
-        # do it like above instead of f strings to avaoid injection attacks
 
 
 def get_entries():
-    # return entries
     cursor = connection.cursor()
-    cursor.execute{"SELECT * FROM entries"}
-    return cursor.fetchall()
+    cursor.execute("SELECT * FROM entries;")
+    return cursor
