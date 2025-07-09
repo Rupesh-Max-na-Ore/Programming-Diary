f3fcf955b6ad25184655780f4c575bbf8afc4c75
Rupesh <dasrupesh2124@gmail.com>
2025-07-09 17:43:58 +0530

    Added database as python lists

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from database import add_entry, view_entries
+
 menu = """Please select one of the following options:
 1) Add new entry for today.
 2) View entries.
@@ -12,7 +14,9 @@ print(WELCOME)
 while (user_input := input(menu)) != "3":
     if user_input == "1":
         print("Adding...")
+        add_entry()
     elif user_input == "2":
         print("Viewing...")
+        view_entries()
     else:
         print("Invalid option, please try again!")
