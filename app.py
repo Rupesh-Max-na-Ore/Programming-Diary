48b3b8a02baef3fb74e59ed31738d5b5884de14f
Rupesh <dasrupesh2124@gmail.com>
2025-07-09 16:58:46 +0530

    Used Walrus operator in while loop to reduce code lines duplication

--- app.py
+++ app.py
@@ -7,8 +7,15 @@ Your selection: """
 
 welcome = "Welcome to the programming diary!"
 
-user_input = input(menu)
-while user_input != '3':
+print(welcome)
 
+# to reduce duplication use walrus notation in pyhton 3.8+
+while (user_input := input(menu)) != '3':
+    # TODO: Deal with user ip here
 
-    user_input = input(menu)
+    if user_input == "1":
+        print("Adding...")
+    elif user_input == "2":
+        print("Viewing...")
+    else:
+        print("Invalid option, please try again!")
