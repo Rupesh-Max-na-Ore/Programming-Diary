1429a48d953a736b9d79f09a1447f64b591383be
Rupesh <dasrupesh2124@gmail.com>
2025-07-09 16:34:50 +0530

    take user input in main app

---
+++ app.py
@@ -0,0 +1,14 @@
+menu = """Please select one of the following options:
+1) Add new entry for today.
+2) View entries.
+3) Exit
+
+Your selection: """
+
+welcome = "Welcome to the programming diary!"
+
+user_input = input(menu)
+while user_input != '3':
+
+
+    user_input = input(menu)
