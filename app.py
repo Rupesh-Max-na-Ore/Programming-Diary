96aaee958f7d584182c0b8338ace563a28df83b0
Rupesh <dasrupesh2124@gmail.com>
2025-07-09 17:19:30 +0530

    removed comments for clarity

--- app.py
+++ app.py
@@ -5,18 +5,14 @@ menu = """Please select one of the following options:
 
 Your selection: """
 
-welcome = "Welcome to the programming diary!"
+WELCOME = "Welcome to the programming diary!"
 
-print(welcome)
+print(WELCOME)
 
-# to reduce duplication use walrus notation in pyhton 3.8+
-while (user_input := input(menu)) != '3':
-    # TODO: Deal with user ip here
-
+while (user_input := input(menu)) != "3":
     if user_input == "1":
         print("Adding...")
     elif user_input == "2":
         print("Viewing...")
     else:
         print("Invalid option, please try again!")
-
